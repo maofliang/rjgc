40cad774272675b4ffcc976f20f463e831562e94
ruangong123 <15216880857@163.com>
2017-04-25 20:53:13 +0800

    Revert "注册和添加商品完成"
    This reverts commit e941140977f2e98e0d55ccdde36d647bb363188c.

--- WEB-INF/src/model/SupplierModel.java
+++ WEB-INF/src/model/SupplierModel.java
@@ -1,50 +1,6 @@
 package model;
 
 public class SupplierModel {
-	private int id=-1;
-	private String name=null;
-	private String telephone=null;
-	private String address=null;
-	private String introduction=null;
-	private String password=null;
-	
-	public void setid(int n){
-		id=n;
-	}
-	public void setname(String s){
-		name=s;
-	}
-	public void settelephone(String s){
-		telephone=s;
-	}
-	public void setaddress(String s){
-		address=s;
-	}
-	public void setintroduction(String s){
-		introduction=s;
-	}
-	public void setpassword(String s){
-		password=s;
-	}
-	
-	public int getid(){
-		return id;
-	}
-	public String getname(){
-		return name;
-	}
-	public String gettelephone(){
-		return telephone;
-	}
-	public String getaddress(){
-		return address;
-	}
-	public String getintroduction(){
-		return introduction;
-	}
-	public String getpassword(){
-		return password;
-	}
-	
-
+	//列出类中的成员变量
+	//参照UserModel类，为类的每个成员变量定义get和set方法
 }
--- WEB-INF/src/service/SupplierService.java
+++ WEB-INF/src/service/SupplierService.java
@@ -1,51 +1,21 @@
 package service;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
+
 import java.sql.SQLException;
 
-import model.*;
-import dao.*;
+import model.SupplierModel;
+
 public class SupplierService {
-	public int SupplierRegister(SupplierModel sModel){
-		java.sql.Connection conn =null;
-		String sName=sModel.getname();
-		String password=sModel.getpassword();
-		ProductDao pDao=new ProductDao();
-		conn=pDao.getConnect();
-		String sql="select Supplierid from supplier where Suppliername=?";
-		try {
-			PreparedStatement ps=conn.prepareStatement(sql);
-			ps.setString(1,sName);
-			ResultSet rst=ps.executeQuery();
-			//鍒ゆ柇鍚勭鎯呭喌
-			if(rst.next()){
-				return -1;
-			}else{
-				sql="insert into supplier(Suppliername,Supplierpassword)values(?,?)";
-				ps=conn.prepareStatement(sql);
-				ps.setString(1, sName);
-				ps.setString(2, password);
-				ps.executeUpdate();
-				sql="select Supplierid from supplier where Suppliername=?";
-				ps=conn.prepareStatement(sql);
-				ps.setString(1, sName);
-				rst=ps.executeQuery();
-				if(rst.next()){
-					int sid=rst.getInt("Supplierid");
-					return sid;
-				}
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		try {
-			conn.close();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-		return -2;
+	public boolean SLoginInfoCheck(String Suppliername,String SupplierPassword) throws SQLException {
+		boolean i = false;
+		SupplierModel supplier=new SupplierModel();
+		//调用SupplierDao中的SLoginCheck方法，验证supplier表中是否有输入的商家
+		return i;
 	}
 	
-	
+	public SupplierModel getSupplierByName(String Suppliername) throws SQLException {
+		SupplierModel supplier=new SupplierModel();
+		//把Suppliername加入supplier对象中
+		//调用SupplierDao的getSupplier(SupplierModel supplier)方法获取完整商家对象
+		return supplier;
+	}
 }
